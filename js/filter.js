465ffd8a365baf5d6f9d1114de0b83083469164f
KPetrosyants <kostya.petrosyants@bk.ru>
2024-11-30 21:01:02 +0300

    фильтр готоов

---
+++ js/filter.js
@@ -0,0 +1,169 @@
+const filterSection = document.querySelector(".filter-section");
+const viewBody = document.querySelector(".view__body");
+const viewItems = viewBody.querySelectorAll(".view__body-item");
+const inputPriceHight = document.querySelector("#max-price");
+const inputPriceLow = document.querySelector("#min-price");
+const assortInputsArea = document.querySelector(".filter__item-details");
+const assortInputs = assortInputsArea.querySelectorAll(".custom-checkbox");
+const typeInputsArea = document.querySelector(".filter__item-details--type");
+const typeInputs = typeInputsArea.querySelectorAll(".custom-checkbox");
+const assortSelect = document.querySelector(
+    ".view__header-options-filters select"
+);
+const filterButton = document.querySelector(
+    ".view__header-options-filter-show"
+);
+
+const availabilitySelect = document.querySelector("#availability");
+
+function filterPrice() {
+    const priceHight = +inputPriceHight.value;
+    const priceLow = +inputPriceLow.value;
+
+    viewItems.forEach((item) => {
+        let price = +item.dataset.price;
+        if (
+            priceLow <= item.dataset.price &&
+            item.dataset.price <= priceHight
+        ) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
+
+function displayItems(elements) {
+    viewBody.innerHTML = ""; // Очистка контейнера
+    elements.forEach((item) => viewBody.appendChild(item)); // Добавление элементов
+}
+function assortTypelow(items) {
+    const newItems = Array.from(items).sort((a, b) => {
+        return (
+            parseFloat(b.getAttribute("data-price")) -
+            parseFloat(a.getAttribute("data-price"))
+        );
+    });
+
+    displayItems(newItems);
+}
+function assortTypeHight(items) {
+    const newItems = Array.from(items).sort((a, b) => {
+        return (
+            parseFloat(a.getAttribute("data-price")) -
+            parseFloat(b.getAttribute("data-price"))
+        );
+    });
+    displayItems(newItems);
+}
+
+function filterType() {
+    const allChecbox = document.getElementById("all-type");
+    const allShow = allChecbox.checked;
+    if (allShow) {
+        viewItems.forEach((item) => {
+            item.style.display = "block";
+        });
+        return;
+    }
+    const activeTypes = Array.from(typeInputs)
+        .filter((checkbox) => checkbox.checked)
+        .map((checkbox) => checkbox.name);
+
+    viewItems.forEach((item) => {
+        // Получаем значения data-type текущего элемента
+        const typeAttr = [item.getAttribute("data-type")];
+
+        // Проверяем, соответствует ли элемент хотя бы одному активному типу
+        const isVisible = typeAttr.some((type) => activeTypes.includes(type));
+
+        // Устанавливаем видимость элемента
+        item.style.display = isVisible ? "block" : "none";
+    });
+}
+
+function filterAssort() {
+    let allShow = false;
+    assortInputs.forEach((checkbox) => {
+        if (checkbox.checked) {
+            allShow = false;
+            return;
+        }
+
+        viewItems.forEach((item) => {
+            item.style.display = "block";
+        });
+        allShow = true;
+    });
+
+    if (allShow) return;
+    const activeWarehouses = Array.from(assortInputs)
+        .filter((checkbox) => checkbox.checked)
+        .map((checkbox) => checkbox.name);
+
+    viewItems.forEach((item) => {
+        // Получаем значения data-assort текущего элемента
+        const assortWarehouses = item.getAttribute("data-assort").split(" ");
+
+        // Проверяем, соответствует ли элемент хотя бы одному активному складу
+        const isVisible = assortWarehouses.some((warehouse) =>
+            activeWarehouses.includes(warehouse)
+        );
+
+        // Устанавливаем видимость элемента
+        item.style.display = isVisible ? "block" : "none";
+    });
+}
+
+function availabilityFilter(string) {
+    viewItems.forEach((item) => {
+        console.log(item.dataset.availability, string);
+
+        item.style.display =
+            item.dataset.availability == string ? "block" : "none";
+    });
+    // if (newItem) {
+    //     displayItems(newItem);
+    // }
+}
+
+assortInputsArea.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.classList.contains("custom-checkbox")) {
+        filterAssort();
+    }
+});
+typeInputsArea.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.classList.contains("custom-checkbox")) {
+        filterType();
+    }
+});
+
+assortSelect.addEventListener("input", () => {
+    const assortType = assortSelect.value;
+
+    switch (assortType) {
+        case "Убыванию стоимости":
+            assortTypelow(viewItems);
+            break;
+        case "Возрастанию стоимости":
+            assortTypeHight(viewItems);
+            break;
+        default:
+            break;
+    }
+});
+
+availabilitySelect.addEventListener("input", () => {
+    const availabilityType = availabilitySelect.value;
+    availabilityFilter(availabilityType);
+});
+
+inputPriceHight.addEventListener("input", filterPrice);
+inputPriceLow.addEventListener("input", filterPrice);
+
+filterButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    filterSection.classList.toggle("show-filter");
+});
--- js/range-script.js
+++ js/range-script.js
@@ -3,21 +3,37 @@ const minInput = document.getElementById("min-price");
 const maxInput = document.getElementById("max-price");
 
 noUiSlider.create(slider, {
-    start: [0, 99999],
+    start: [0, 50000],
+    tooltips: [true, true],
     connect: true,
     range: {
         min: 0,
-        max: 99999,
+        max: 50000,
     },
     step: 1,
+    format: {
+        // 'to' the formatted value. Receives a number.
+        to: function (value) {
+            return value + "";
+        },
+        // 'from' the formatted value.
+        // Receives a string, should return a number.
+        from: function (value) {
+            return Math.floor(Number(value.replace(".", ""))); // Убираем все после запятой и преобразуем в целое число
+        },
+    },
 });
-
+const tooltip = document.querySelectorAll(".noUi-tooltip");
 slider.noUiSlider.on("update", function (values, handle) {
     if (handle === 0) {
         minInput.value = Math.round(values[0]);
     } else {
         maxInput.value = Math.round(values[1]);
     }
+    tooltip.forEach((el) => {
+        el.innerHTML = Math.floor(Number(el.innerHTML));
+    });
+    filterPrice();
 });
 
 minInput.addEventListener("change", function () {
