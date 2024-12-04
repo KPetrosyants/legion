c57dbe5617a0cf4850f712810fa36e3c2d89b048
KPetrosyants <kostya.petrosyants@bk.ru>
2024-12-04 12:32:01 +0300

    Подключил библеотеку и настроил select

---
+++ js/custom-selects.js
@@ -0,0 +1,18 @@
+new SlimSelect({
+    select: "#offers",
+    settings: {
+        showSearch: false, // used in example
+    },
+});
+new SlimSelect({
+    select: "#availability",
+    settings: {
+        showSearch: false, // used in example
+    },
+});
+new SlimSelect({
+    select: "#view-filter",
+    settings: {
+        showSearch: false, // used in example
+    },
+});
